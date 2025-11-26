a736a5ba1d097d48f79398272ef8675117d45dde
Jan Armbrust <jan.armbrust@googlemail.com>
2025-11-26 13:51:06 +0100

    Initial commit

---
+++ PhotoEditor/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PhotoEditor
+//
+//  Created by Jan Armbrust on 01.07.2025.
+//
+
+import StoreKit
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ProductView(id: "dev.xbow.PhotoEditor.BwFilter")
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Jan Armbrust on 01.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
